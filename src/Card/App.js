af0bd76a48eab8b5edaa61f8aff4ea84e0a49e59
Md Arman <106583530+arman-arian12@users.noreply.github.com>
2022-11-18 15:58:46 +0600

    Card-design
    Card-design using react

---
+++ src/Card/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from './components/Card'
+import Toggle from './components/Toggle/Toggle';
+
+function App(){
+    return <div>
+        {/* <div className='main'>
+      <h1 className='headings'>Welcome</h1> 
+      <Card />
+      <Card />
+      <Card />
+      
+</div>  */}
+    <Toggle />
+
+    </div>
+    
+};
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
